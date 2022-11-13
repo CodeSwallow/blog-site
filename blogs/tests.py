ec542da1c90652c32d89c74247e0aeb29a7a84ed
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2022-11-13 10:56:23 -0600

    added post model and test

--- blogs/tests.py
+++ blogs/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+from blogs.models import Post
+
+
+class PostModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Post.objects.create()
+
+    def test_get_absolute_url(self):
+        post = Post.objects.get(id=1)
+        self.assertEqual(post.get_absolute_url(), '/post/1')
