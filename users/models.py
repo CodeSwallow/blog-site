3fd0fbdaaeaf0a529eac4b5d3ca3ba6f6b2d4032
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2022-11-13 12:15:21 -0600

    added class meta and method to user

--- users/models.py
+++ users/models.py
@@ -1,6 +1,14 @@
 from django.contrib.auth.models import AbstractUser
+from django.shortcuts import reverse
 
 
 class User(AbstractUser):
-    """Custom user for easier modification"""
-    pass
+    """Custom user for easier modification."""
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{self.last_name}, {self.first_name}'
+
