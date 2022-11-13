1dd684de89600474d043748a860460ff43480270
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2022-11-13 13:35:25 -0600

    removed redundant docstring

--- users/tests.py
+++ users/tests.py
@@ -11,7 +11,6 @@ class UserModelTest(TestCase):
         User.objects.create_user(username="user_1", password="password_123")
 
     def test_object_name_is_last_name_comma_first_name(self):
-        """Test object name has 'last name', 'first name' format."""
         user = User.objects.get(id=1)
         expected_object_name = f'{user.last_name}, {user.first_name}'
         self.assertEqual(str(user), expected_object_name)
