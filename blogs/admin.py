4d461abe33d6af2b1efd6fcfcd442eacdb3f517c
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2022-11-13 13:36:11 -0600

    registered all blogs app models

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
-from blogs.models import Post
+from blogs.models import (
+    Post,
+    Category,
+    Series
+)
 
 admin.site.register(Post)
+admin.site.register(Category)
+admin.site.register(Series)
