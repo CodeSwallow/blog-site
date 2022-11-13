9077ff6086b24a8328915c5511bc4c23cfb7db02
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2022-11-13 12:15:54 -0600

    added post model method

--- blogs/models.py
+++ blogs/models.py
@@ -1,5 +1,10 @@
 from django.db import models
+from django.shortcuts import reverse
 
 
 class Post(models.Model):
-    pass
+    """Model for blog posts"""
+
+    def get_absolute_url(self):
+        """Returns the URL to access a particular user instance."""
+        return reverse('post-detail', args=[str(self.id)])
