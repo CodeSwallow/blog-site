d597b214a39ef85968f2b6e2a6ecd69a240d4cb1
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2022-11-13 13:28:21 -0600

    added docstrings

--- users/tests.py
+++ users/tests.py
@@ -4,11 +4,14 @@ from users.models import User
 
 
 class UserModelTest(TestCase):
+    """Test for custom user model."""
+
     @classmethod
     def setUpTestData(cls):
         User.objects.create_user(username="user_1", password="password_123")
 
     def test_object_name_is_last_name_comma_first_name(self):
+        """Test object name has 'last name', 'first name' format."""
         user = User.objects.get(id=1)
         expected_object_name = f'{user.last_name}, {user.first_name}'
         self.assertEqual(str(user), expected_object_name)
