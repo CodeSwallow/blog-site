410296078e8c15bd9120911e71eba78e78391689
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2022-11-14 00:19:02 -0600

    update url names

--- blogs/urls.py
+++ blogs/urls.py
@@ -2,11 +2,14 @@ from django.urls import path
 
 from blogs import views
 
+app_name = 'blogs'
+
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
-    path('post/<slug:slug>', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/<slug:slug>', views.PostDetailView.as_view(), name='post'),
     path('series/', views.SeriesListView.as_view(), name='series-list'),
     path('series/<slug:slug>', views.SeriesDetailView.as_view(), name='series-detail'),
-    path('categories/', views.CategoryListView.as_view(), name='list'),
-    path('category/<slug:slug>', views.CategoryDetailView.as_view(), name='category-detail'),
+    path('categories/', views.CategoryListView.as_view(), name='categories'),
+    path('category/<slug:slug>', views.CategoryDetailView.as_view(), name='category'),
+    path('search/', views.SearchResultsView.as_view(), name='search'),
 ]
