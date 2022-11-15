966123e00407858787fffa868d6987d6b0906d7a
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2022-11-15 16:56:22 -0600

    added comment form to PostView

--- blogs/urls.py
+++ blogs/urls.py
@@ -6,7 +6,7 @@ app_name = 'blogs'
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
-    path('post/<slug:slug>', views.PostDetailView.as_view(), name='post'),
+    path('post/<slug:slug>', views.PostView.as_view(), name='post'),
     path('series/', views.SeriesListView.as_view(), name='series-list'),
     path('series/<slug:slug>', views.SeriesDetailView.as_view(), name='series-detail'),
     path('categories/', views.CategoryListView.as_view(), name='categories'),
--- blogs/views.py
+++ blogs/views.py
@@ -1,14 +1,16 @@
-from django.views import generic
+from django.views import generic, View
+from django.views.generic import FormView
+from django.views.generic.detail import SingleObjectMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse
 from django.db.models import Q
 
-from blogs.models import (
-    Post,
-    Series,
-    Category
-)
+from blogs.models import Post, Series, Category
+from blogs.forms import PostCommentForm
 
 
 class HomePageView(generic.ListView):
+    # TODO: session for dark/light mode
     queryset = Post.objects.all().order_by('-pub_date')
     template_name = 'blogs/homepage.html'
     paginate_by = 10
@@ -21,6 +23,45 @@ class HomePageView(generic.ListView):
 
 class PostDetailView(generic.DetailView):
     model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = PostCommentForm()
+        return context
+
+
+class PostCommentFormView(LoginRequiredMixin, SingleObjectMixin, FormView):
+    # TODO: Redirect unauthenticated user to login
+    # TODO: Allow user to reply to a comment
+    # TODO: Allow edit/deletion of comment
+    template_name = 'blogs/post_detail.html'
+    form_class = PostCommentForm
+    model = Post
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super().post(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        f = form.save(commit=False)
+        f.author = self.request.user
+        f.post = self.object
+        f.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('blogs:post', kwargs={'slug': self.object.slug}) + '#comments-section'
+
+
+class PostView(View):
+
+    def get(self, request, *args, **kwargs):
+        view = PostDetailView.as_view()
+        return view(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        view = PostCommentFormView.as_view()
+        return view(request, *args, **kwargs)
 
 
 class SeriesListView(generic.ListView):
